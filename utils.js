c017bdab984bc395254645fb57f52dd53fabb709
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-02-29 14:20:14 +0200

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,24 @@
+export const create=(obj,args=[])=>Reflect.construct(obj,args);
+export const $=document.querySelector.bind(document);
+export const h=document.createElement.bind(document);
+export const p=(a,b)=>a.appendChild(b);
+export const wait=tm=>create(Promise,[
+ res=>setTimeout(res,tm)
+]);
+export const pipe=async(v,arr)=>(
+ await(arr.reduce(
+  async(p,f)=>await(f(await(p)))
+  ,Promise.resolve(v)
+ ))
+);
+export const throttle=(func,tm=0,{
+ beforeEach=()=>{},
+ afterLast=()=>{}
+},to)=>arg=>{
+ beforeEach(arg);
+ clearTimeout(to);
+ to=setTimeout(async(arg)=>{
+  await(func(arg));
+  afterLast(arg);
+ },tm,arg);
+};
