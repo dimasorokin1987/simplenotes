101f900be212f9ca1b891f089fc3f64bcf428e13
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-02-29 14:12:34 +0200

    Create test/index.js

---
+++ test/index.js
@@ -0,0 +1,7 @@
+import{testLog}from'test/log';
+import{testCustomImport}from'test/customImport';
+import{testCryptoAuth}from'test/crypto/auth';
+  
+testLog();
+testCryptoAuth();
+testCustomImport();
