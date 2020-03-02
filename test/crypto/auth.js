d4dd6116cff1cf010ff34d64784be8d3a4225c73
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-03-02 22:41:10 +0200

    Update auth.js
    comment crypto local store and load

--- test/crypto/auth.js
+++ test/crypto/auth.js
@@ -152,7 +152,7 @@ log(7,{obj});
 
 }catch(e){alert(e)}})();
 */
-
+/*
 log(8,{test:'crypto local store and load func'});
 (async()=>{try{
 const{
@@ -169,5 +169,6 @@ const obj=await(cryptoLocalLoad(
 log(8,{obj});
 
 }catch(e){alert(e)}})();
+*/
 
 };
