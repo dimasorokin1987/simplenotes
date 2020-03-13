6e80fcde134bb0da28264971734ae3e439ebce97
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-03-13 20:10:18 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install',event=>{
+ event.waitUntil(
+  caches.open('v1').then(cache=>(
+   cache.addAll([
+    '/simplenotes/',
+    '/simplenotes/index.html',
+    '/simplenotes/utils.js',
+    '/simplenotes/log.js',
+    '/simplenotes/crypto/auth.js',
+    '/simplenotes/ui/auth.js',
+    '/simplenotes/customImport.js',
+    '/simplenotes/test/index.js',
+    '/simplenotes/test/ui/auth.js',
+    '/simplenotes/test/crypto/auth.js',
+    '/simplenotes/test/log.js',
+    '/simplenotes/test/customImport.js'
+   ]);
+  ))
+ );
+});
+
+self.addEventListener('fetch',async(event)=>{
+  let url=new URL(event.request.url);
+  let responseP=caches.match(url);
+  let response=null;
+  if(responseP){
+   response=await(responseP);
+  }else{
+   response=await(fetch(event.request));
+   let responseClone=response.clone();
+   let cache=await(caches.open('v1'));
+   cache.put(event.request,responseClone);
+  }
+  event.respondWith(response);
+});
