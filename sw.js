2b27f4fc50e82dfc7d2363c4b5fe81aa32dfaf49
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-04-09 12:54:03 +0300

    Update sw.js
    update version functionality

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
+const version='v2';
 let logs=[];
 let errors=[];
 
-const versionUrl='/simplenotes/version';
 const urlsToCache=[
  '/simplenotes/cache.appcache',
  '/simplenotes/index.html',
@@ -9,56 +9,31 @@ const urlsToCache=[
  '/simplenotes/utils.js',
  '/simplenotes/log.js',
  '/simplenotes/crypto/auth.js',
- '/simplenotes/ui/auth.js'
+ '/simplenotes/ui/auth.js',
+ '/simplenotes/sw.js'
 ];
 
-const fetchVersion=async(r,v)=>{
- r=await(fetch(versionUrl));
- v=await(r.text());
- return(v);
-};
-
-const cacheResources=async(
- version,cache
-)=>{
+const cacheResources=async()=>{
  if(navigator.onLine){
-  if(version===undefined){
-   version=await(fetchVersion());
-  }
-  cache=await(caches.open(version));
+  const cache=await(caches.open(version));
   await(cache.addAll(urlsToCache));
   logs.push('cached resources version:'+version);
  }else{
-  logs.push('cache resourses: offline');
+  errors.push('cache resourses: offline');
  }
 };
 
-const updateCache=async(
- currentVersion,
- cacheNames,
- arpr,r
-)=>{
- try{
-  if(navigator.onLine){
-   currentVersion=await(fetchVersion());
-   cacheNames=await(caches.keys());
-   arpr=cacheNames
-   .filter(cn=>(cn!==currentVersion))
-   .map(cn=>{caches.delete(cacheName)});
-   r=await(Promise.all(arpr));
-   logs.push('cashes delete ok:'+r);
-   if(!caches.has(currentVersion)){
-    await(cacheResources(currentVersion));
-    logs.push('cache new version ok');
-   }
-  }else{
-   logs.push('update cache: offline');
-  }
- }catch(error){errors.push(error)}
-};
+const updateCache=async()=>{try{
+ const cacheNames=await(caches.keys());
+ const arpr=cacheNames
+ .filter(cn=>(cn!==version))
+ .map(cn=>caches.delete(cn));
+ const r=await(Promise.all(arpr));
+ logs.push('cashes delete ok:'+r);
+}catch(error){errors.push(error)}};
 
-self.addEventListener(
- 'install',async(event)=>{try{
+self.addEventListener('install',
+ async(event)=>{try{
  // event.waitUntil(self.skipWaiting());
   logs.push('service worker install');
   event.waitUntil(
@@ -67,16 +42,18 @@ self.addEventListener(
  }catch(error){errors.push(error)}}
 );
 
-self.addEventListener('activate',event=>{
- logs.push('service worker activate');
- //event.waitUntil(self.skipWaiting());
- event.waitUntil(
-  updateCache()
- );
-});
+self.addEventListener('activate',
+ event=>{
+  logs.push('service worker activate');
+  //event.waitUntil(self.skipWaiting());
+  event.waitUntil(
+   updateCache()
+  );
+ }
+);
 
-self.addEventListener(
- 'fetch',async(event)=>{
+self.addEventListener('fetch',
+ async(event)=>{
   logs.push(event.request.url);
   let url=new URL(event.request.url);
   logs.push(url);
@@ -85,10 +62,15 @@ self.addEventListener(
   if(responseP){
    response=await(responseP);
   }else{
-   response=await(fetch(event.request));
-   let responseClone=response.clone();
-   let cache=await(caches.open('v1'));
-   cache.put(event.request,responseClone);
+   if(navigator.onLine){
+    response=await(fetch(event.request));
+    let responseClone=response.clone();
+    let cache=await(caches.open(version));
+    await(cache.put(event.request,responseClone));
+   }else{
+    response='';
+    errors.push('fetch: cache resourse: offline');
+   }
   }
   event.respondWith(response);
  }
